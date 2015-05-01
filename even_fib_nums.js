5dd28eace77e34269e9e8d3c1ee5f60a56926ee6
Sean Nakamura <seanenakamura@gmail.com>
2015-04-30 21:49:07 -1000

    first-iteration

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -1,6 +1,27 @@
 exports.sumOfEvenFibonacciNumbers = function(n){
   var sum = 0;
-  // do your work here
-  
+  var fib = [];
+
+  fib[0] = 0;
+  fib[1] = 1;
+  for (var i = 2; i < Infinity; i++) {
+    // fib[i] = fib[i-2] + fib[i-1];
+    // console.log(fib[i]);
+    var newFib = fib[i-2] + fib[i-1];
+    if ( newFib > n) {
+      break;
+    }else{
+      fib[i] = newFib;
+    }
+  }
+  // console.log(fib);
+  for (var j = 0; j < fib.length; j++) {
+    if (fib[j] % 2 === 0){
+      sum += fib[j];
+      console.log(sum);
+    }
+
+  }
+
   return sum;
 };
--- test/even_fib_nums_spec.js
+++ test/even_fib_nums_spec.js
@@ -1,20 +1,20 @@
 var challenge_2 = require('./../even_fib_nums.js');
 var chai = require('chai');
-var should = chai.should;
+var should = chai.should();
 
 describe("Sum of even numbers in the Fibonnacci sequence", function () {
 
   it("should return the sum of all even Fibonacci numbers when given 10", function () {
-    var sum = challenge_2.sumOfEvenFibonacciNumbers(10);
-    sum.should.equal(10);
+    var sum = challenge_2.sumOfEvenFibonacciNumbers(34);
+    sum.should.equal(44);
   });
 
   // when your tests pass, uncomment the next test and
   // change the expected value to the correct result
 
-  // it("should return the sum of all even Fibonacci numbers when given 4,000,000", function () {
-  //   var sum = challenge_2.sumOfEvenFibonacciNumbers(4000000);
-  //   sum.should.equal('X'); // CHANGE THE VALUE OF 'X' to the correct number
-  // });
+  it("should return the sum of all even Fibonacci numbers when given 4,000,000", function () {
+    var sum = challenge_2.sumOfEvenFibonacciNumbers(4000000);
+    sum.should.equal(4613732); // CHANGE THE VALUE OF 'X' to the correct number
+  });
 
 });
